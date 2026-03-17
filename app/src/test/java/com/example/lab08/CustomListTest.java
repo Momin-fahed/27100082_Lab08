fcce7dab782e3356225d3be971424607f1052c7b
Momin-Fahed <27100082@lums.edu.pk>
2026-03-17 14:20:20 +0500

    Added failing test for hasCity

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,17 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
 
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        // Should FAIL (method not implemented yet)
+        assertTrue(list.hasCity(calgary));
+    }
 }
